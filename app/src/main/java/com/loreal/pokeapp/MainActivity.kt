6d22781392a9369e824180f87b6d86dd162b1519
Pedro Loureiro <pedro.loureiro@luggit.app>
2026-04-26 12:24:57 +0100

    refactor: sanitizing main branch

--- app/src/main/java/com/loreal/pokeapp/MainActivity.kt
+++ app/src/main/java/com/loreal/pokeapp/MainActivity.kt
@@ -1,82 +1,29 @@
 package com.loreal.pokeapp
 
-import android.R.attr.label
 import android.os.Bundle
-import android.text.TextUtils.replace
-import android.view.Surface
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
-
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.GridItemSpan
-import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.Star
-import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.DockedSearchBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SearchBar
-import androidx.compose.material3.SearchBarColors
-import androidx.compose.material3.SearchBarDefaults
-import androidx.compose.material3.Shapes
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.input.key.Key.Companion.G
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.semantics.semantics
-import androidx.compose.ui.semantics.traversalIndex
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.loreal.pokeapp.ui.OnboardingScreen
-import com.loreal.pokeapp.ui.components.Greetings
 import com.loreal.pokeapp.ui.theme.PokeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -109,106 +56,13 @@ fun AppBar() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Content(innerPadding: PaddingValues) {
-    val textFieldState: TextFieldState = TextFieldState()
-
     Column(
         modifier = Modifier.padding(innerPadding)
             .padding(vertical = 16.dp)
             .verticalScroll(rememberScrollState()),
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-        SearchBar(
-            modifier = Modifier.fillMaxWidth(1f).padding(horizontal = 16.dp),
-            inputField = {
-                SearchBarDefaults.InputField(
-                    query = textFieldState.text.toString(),
-                    leadingIcon = {
-                        Icon(
-                            imageVector = Icons.Default.Search,
-                            contentDescription = "Search"
-                        )
-                    },
-                    onQueryChange = { textFieldState.edit { replace(0, length, it) } },
-                    onSearch = { },
-                    expanded = false,
-                    onExpandedChange = { },
-                    placeholder = { Text("Search") }
-                )
-            },
-            expanded = false,
-            onExpandedChange = { },
-        ) {}
-
-        Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
-            Text("Align your body", modifier = Modifier.padding(horizontal = 16.dp))
-            LazyRow(
-                contentPadding = PaddingValues(horizontal = 16.dp),
-                horizontalArrangement = Arrangement.spacedBy(8.dp)
-            ) {
-                item { CircleImage(label = "Yoga") }
-                item { CircleImage(label = "Quick yoga") }
-                item { CircleImage(label = "Stretching") }
-                item { CircleImage(label = "Tabata") }
-            }
-        }
-
-        Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
-            Text("Favorite collections", modifier = Modifier.padding(horizontal = 16.dp))
-            LazyHorizontalGrid(
-                rows = GridCells.Fixed(2),
-                contentPadding = PaddingValues(horizontal = 16.dp),
-                horizontalArrangement = Arrangement.spacedBy(16.dp),
-                verticalArrangement = Arrangement.spacedBy(16.dp),
-                modifier = Modifier.height(168.dp)
-            ) {
-                item { ExerciseItem(label = "Yoga") }
-                item { ExerciseItem(label = "Quick Yoga") }
-                item { ExerciseItem(label = "Stretching") }
-                item { ExerciseItem(label = "Tabata") }
-            }
-        }
     }
-}
-
-@Composable
-fun CircleImage(label: String? = null) {
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        Surface(
-            modifier = Modifier
-                .size(102.dp)
-                .border(width = 1.dp, color = Color.Black, shape = CircleShape)
-        ) {
-            Image(
-                imageVector = Icons.Default.Star,
-                contentDescription = "Circle Image",
-                modifier = Modifier.fillMaxSize()
-            )
-        }
-        label?.let { Text(it) }
-    }
-}
-
-@Composable
-fun ExerciseItem(label: String? = null) {
-    Surface(
-        shape = MaterialTheme.shapes.medium,
-        color = MaterialTheme.colorScheme.surfaceVariant,
-    ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.spacedBy(16.dp),
-            modifier = Modifier.width(255.dp)
-        ) {
-            Image(
-                imageVector = Icons.Default.Star,
-                contentDescription = "Image",
-                contentScale = ContentScale.Crop,
-                modifier = Modifier.height(80.dp)
-            )
-            label?.let { Text(it) }
-        }
-    }
-
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
